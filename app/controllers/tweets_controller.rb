9af9c4b23e990987e57b2edf24908b8f2e38f102
Kohei Sato <Kohei Sato>
2016-05-03 14:08:35 +0900

    post about!?

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,12 @@ def index
 @tweets = Tweet.all
 end
 
+def new
+end
 
+def create
+  Tweet.create(name:params[:name], drink_name:params[:drimk_name], custom_name:params[:custom_name], image: params[:image])
+end
 
 
 
