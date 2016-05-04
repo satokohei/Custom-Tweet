87936169b4696f6ce5ebfc060850e51ff764f4c2
Kohei Sato <Kohei Sato>
2016-05-04 12:46:05 +0900

    いったんデザインへ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
 def show
-  user = User.find(params[:id])
-  @tweets = user.tweets
+  @user = User.find(params[:id])
+  @tweets = @user.tweets
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503080358) do
+ActiveRecord::Schema.define(version: 20160504032704) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160503080358) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
